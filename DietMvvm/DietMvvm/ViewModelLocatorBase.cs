f72057fdcd3eeddccf30d9516859c9598e072f0b
Ashok Gelal <ashok@metageek.net>
2012-03-16 01:49:36 -0600

    Add IoC

---
+++ DietMvvm/DietMvvm/ViewModelLocatorBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Practices.Unity;
+
+namespace DietMvvm
+{
+    public abstract class ViewModelLocatorBase
+    {
+        protected static IUnityContainer _container;
+
+        static ViewModelLocatorBase()
+        {
+            _container = new UnityContainer();
+        }
+    }
+}
